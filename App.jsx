9c4cf297ae50b18d7814a279d2ddc8adcf61ef57
Abhishek Sinha <catchabhi@live.com>
2017-04-27 09:32:36 +0530

    First Component Demo

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class App extends React.Component{
+    render(props){
+        return(
+        <div>
+            <p> Hello from paragraph </p>
+            Hello World
+        </div>
+        )
+    }
+}
+
+export default App;
