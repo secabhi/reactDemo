bc5098be54a8b4c43d27510c28721f7283bbc0d7
Abhishek Sinha <catchabhi@live.com>
2017-05-05 09:04:06 +0530

    react with events demo

--- App.jsx
+++ App.jsx
@@ -1,15 +1,104 @@
 import React from 'react';
 
+//import First from './first.jsx';
+
 
 class App extends React.Component{
-    render(props){
+    constructor(){
+        super()
+        this.state={
+            data:[{
+                "id":1,
+                "name":"Naveen",
+                "age":"20"
+            },{
+                "id":2,
+                "name":"Siddhu",
+                "age":"22"
+            },{
+                "id":3,
+                "name":"Abhi",
+                "age":"23"
+            },{
+                "id":4,
+                "name":"Ramesh",
+                "age":"24"
+            }]
+        }
+        this.custom = this.custom.bind(this);
+    }
+
+    custom(e){
+
+      console.log("called");
+      console.log(e);
+      // change data here and call this.setState({"data":this.state.data})
+    }
+
+    render( ){
+
+        return(<span>
+               <Header/>
+              <table>
+                    <tbody>
+                    {this.state.data.map((person,i)=>
+                    <TableRow key={i} data={person} events={this.custom} />)}
+                    </tbody>
+                </table>
+             <Footer />
+            </span>)
+
+    }
+}
+
+// Stateless components
+
+class TableRow extends React.Component{
+    render(){
         return(
-        <div>
-            <p> Hello from paragraph </p>
-            Hello World
-        </div>
+            <tr  style={{color:"red"}} onClick={this.props.events}>
+                <td>{this.props.data.id}</td>
+                <td>{this.props.data.name}</td>
+                <td>{this.props.data.age}</td>
+            </tr>
+        )
+    }
+}
+
+
+class Header extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>This is Header</h1>
+            </div>
+        )
+    }
+}
+
+
+class Footer extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>This is Footer</h1>
+            </div>
+        )
+    }
+}
+
+class Content extends React.Component{
+    render(){
+        return(
+            <div>
+                <table>
+                    <tbody></tbody>
+                </table>
+            </div>
         )
     }
 }
 
 export default App;
+
+
---
+++ first.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const First = (props) => {
+    return (
+        <span>
+                <p className="divClass"> {props.value} </p>
+                <div> Hello again </div>
+                <div> Hello again </div>
+        </span>
+
+    )
+}
+
+export default First
